cc50a365d8b84da9f430ed8a618908ab40c5dbf8
Harsha Vardhan koneru <harsha.koneru@lsz.ads.valuelabs.net>
2019-03-20 20:15:23 +0530

    searchName Added

--- config/values.ts
+++ config/values.ts
@@ -2,4 +2,5 @@ export class Values{
     emailId = 'koneru.harshavardhan499@gmail.com';
     password = 'Secure@45';
     wrongPassword = 'Abc123456789';
+    searchName = 'Vamsi Krishna Koneru';
 }
