8afe93df6dd7f363b3fc2786e8af229b4d27e6d0
Harsha Vardhan koneru <harsha.koneru@lsz.ads.valuelabs.net>
2019-03-20 17:43:36 +0530

    LinkedIn Login Function elements are added

---
+++ pages/elementsPage.ts
@@ -0,0 +1,14 @@
+export class ElementsPage{
+    //Email Address Element
+    get emailAddress(){
+        return browser.$('#login-email')
+    }
+    // Password Element
+    get password(){
+        return browser.$('#login-password')
+    }
+    // SignIn Element
+    get signIn(){
+        return browser.$('#login-submit')
+    }
+}
